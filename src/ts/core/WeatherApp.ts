7c088f086f1ff53129107b8a2a35c0e282dfdbae
Jiří <satorajirka@seznam.cz>
2024-10-19 16:26:31 +0200

    begin to work on WeatherApp class

--- src/ts/core/WeatherApp.ts
+++ src/ts/core/WeatherApp.ts
@@ -1,5 +1,73 @@
-class WeatherApp {
+import CurrentWeatherData from "./data/CurrentWeatherData";
+import LocationSearch from "./data/LocationSearch";
+import WeatherDataLoader from "./data/WeatherDataLoader";
+import EventSourcePoint from "./utils/EventSourcePoint";
 
+class WeatherApp {
+    private weatherDataLoader: WeatherDataLoader;
+    private locationSearch: LocationSearch;
+
+    private onCurrentWeatherLoadingStartedEventSource = new EventSourcePoint<undefined>();
+    private onCurrentWeatherLoadedEventSource = new EventSourcePoint<CurrentWeatherData>();
+    private onCurrentWeatherLoadingErrorEventSource = new EventSourcePoint<undefined>();
+
+    private latitude: number;
+    private longitude: number;
+    private language: string;
+    private currentWeatherAbortController: AbortController | null = null;
+
+    constructor(weatherDataLoader: WeatherDataLoader, locationSearch: LocationSearch, latitude: number, longitude: number, language: string) {
+        this.weatherDataLoader = weatherDataLoader;
+        this.locationSearch = locationSearch;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.language = language;
+    }
+
+    public async loadCurrentWeather() : Promise<undefined> {
+        if (this.currentWeatherAbortController) {
+            this.currentWeatherAbortController.abort();
+        } else {
+            this.onCurrentWeatherLoadingStartedEventSource.fire(undefined);
+        }
+
+        this.currentWeatherAbortController = new AbortController();
+
+        try {
+            const currentWeatherData = await this.weatherDataLoader.loadCurrentWeatherData(this.latitude, this.longitude, this.language, this.currentWeatherAbortController.signal);
+            this.onCurrentWeatherLoadedEventSource.fire(currentWeatherData);
+        } catch(err) {
+            if (err instanceof Error && err.name === "AbortError") return;
+            console.error(err);
+            this.onCurrentWeatherLoadingErrorEventSource.fire(undefined);
+        }
+
+        this.currentWeatherAbortController = null;
+    }
+
+    public addOnCurrentWeatherLoadingStartedListener(callback: () => void): void {
+        this.onCurrentWeatherLoadingStartedEventSource.subscribe(callback);
+    }
+
+    public addOnCurrentWeatherLoadedListener(callback: (data: CurrentWeatherData) => void): void {
+        this.onCurrentWeatherLoadedEventSource.subscribe(callback);
+    }
+
+    public addOnCurrentWeatherLoadingErrorListener(callback: () => void): void {
+        this.onCurrentWeatherLoadingErrorEventSource.subscribe(callback);
+    }
 }
 
 export default WeatherApp;
+
+/*
+
+
+Takže o co se to bude starat?
+- o načítání aktuálního počasí a předpovědi
+    - zbytek už by mělo být na gui, jak si to zpracuje a zobrazí
+- bude si to držet nastavený jazyk, město...
+    - gui může reagovat na tyto změny
+
+
+*/
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,3 +1,4 @@
+import WeatherApp from "./core/WeatherApp";
 import OpenWeatherMapDataLoader from "./data/OpenWeatherMapDataLoader";
 import OpenWeatherMapLocationSearch from "./data/OpenWeatherMapLocationSearch";
 
@@ -5,6 +6,17 @@ const OPEN_WEATHER_MAP_APP_ID = "610f182df978d44c879ed39ce0f7f9a9";
 
 const weatherDataLoader = new OpenWeatherMapDataLoader(OPEN_WEATHER_MAP_APP_ID);
 const locationSearch = new OpenWeatherMapLocationSearch(OPEN_WEATHER_MAP_APP_ID);
+
+const weatherApp = new WeatherApp(weatherDataLoader, locationSearch, 49.68128712714686, 17.04514591890507, "cz");
+weatherApp.addOnCurrentWeatherLoadedListener(data => {
+    console.log(data);
+    console.log("loading ended");
+});
+weatherApp.addOnCurrentWeatherLoadingStartedListener(() => {
+    console.log("loading started");
+});
+
+weatherApp.loadCurrentWeather();
 
 /*async function test() {
     const data = await weatherDataLoader.loadCurrentWeatherData(49.68128712714686, 17.04514591890507, "cz");
